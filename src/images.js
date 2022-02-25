f1c4d62409cf0746d6b2ce74e53ddd671999b24e
rbanda1985 <robertbanda85@gmail.com>
2022-02-25 05:29:08 -0600

    ADD: images.js with images for carousel

---
+++ src/images.js
@@ -0,0 +1,100 @@
+const images = [
+  {
+    id: 1,
+    image: "./assets/gallery1.jpg"
+  },
+  {
+    id: 2,
+    image: "./assets/gallery2.jpg"
+  },
+  {
+    id: 3,
+    image: "./assets/gallery4.jpg"
+  },
+  {
+    id: 4,
+    image: "./assets/gallery5.jpg"
+  },
+  {
+    id: 5,
+    image: "./assets/gallery6.jpg"
+  },
+  {
+    id: 6,
+    image: "./assets/gallery7.jpg"
+  },
+  {
+    id: 7,
+    image: "./assets/gallery8.jpg"
+  },
+  {
+    id: 8,
+    image: "./assets/gallery9.jpg"
+  },
+  {
+    id: 9,
+    image: "./assets/gallery10.jpg"
+  },
+  {
+    id: 10,
+    image: "./assets/gallery11.jpg"
+  },
+  {
+    id: 11,
+    image: "./assets/gallery12.jpg"
+  },
+  {
+    id: 12,
+    image: "./assets/gallery13.jpg"
+  },
+  {
+    id: 13,
+    image: "./assets/gallery14.jpg"
+  },
+  {
+    id: 14,
+    image: "./assets/gallery15.jpg"
+  },
+  {
+    id: 15,
+    image: "./assets/gallery16.jpg"
+  },
+  {
+    id: 16,
+    image: "./assets/gallery17.jpg"
+  },
+  {
+    id: 17,
+    image: "./assets/gallery18.jpg"
+  },
+  {
+    id: 18,
+    image: "./assets/gallery19.jpg"
+  },
+  {
+    id: 19,
+    image: "./assets/gallery20.jpg"
+  },
+  {
+    id: 20,
+    image: "./assets/gallery21.jpg"
+  },
+  {
+    id: 21,
+    image: "./assets/gallery22.jpg"
+  },
+  {
+    id: 22,
+    image: "./assets/gallery23.jpg"
+  },
+  {
+    id: 23,
+    image: "./assets/gallery24.jpg"
+  },
+  {
+    id: 24,
+    image: "./assets/gallery25.jpg"
+  }
+];
+
+export default images;
